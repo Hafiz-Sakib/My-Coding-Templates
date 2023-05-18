9b6e9ac87b04975970096831c46f6830a23b8e09
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-18 18:52:32 +0600

    Prime Factorization

--- Number Theory/1_to_N__allNumbersPrimeFactorization.cpp
+++ Number Theory/1_to_N__allNumbersPrimeFactorization.cpp
@@ -4,44 +4,13 @@ using namespace std;
 typedef long long int ll;
 const int mx = 1e8;
 
-bitset<mx> is_prime;
-vector<int> primes;
-
-void sieve(int n)
-{
-    for (int i = 3; i <= n; i += 2)
-    {
-        is_prime[i] = 1;
-    }
-
-    for (int i = 3; (i * i) <= n; i += 2)
-    {
-        if (is_prime[i])
-        {
-            for (int j = (i * i); j <= n; j += (i + i))
-            {
-                is_prime[j] = 0;
-            }
-        }
-    }
-
-    is_prime[1] = 0;
-    is_prime[2] = 1;
-    primes.push_back(2);
-
-    for (int i = 3; i <= n; i += 2)
-    {
-        if (is_prime[i])
-        {
-            primes.push_back(i);
-        }
-    }
-}
+vector<int> primes = {2, 3, 5, 7, 11};
 
 vector<vector<int>> PrimeFactors(mx);
 
 void PrimeFactorization(int n)
 {
+    // Can Sieve First
     for (auto p : primes)
     {
         for (int i = p; i <= n; i += p)
@@ -63,11 +32,9 @@ void PrimeFactorization(int n)
 
 int main()
 {
-    sieve(1e5);
+    PrimeFactorization(15);
 
-    PrimeFactorization(1000);
-
-    // 1 to 1000 all numbers Prime Factorization
+    // 1 to 15 all numbers Prime Factorization
 
     return 0;
 }
