cbae57f3d95f79af27c2f2f461be3e07adc9f14d
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-18 19:46:12 +0600

    Updates

--- Number Theory/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-    for (int i = 1; i <= n; i++)
-    {
-        cout << i << " : ";
-        for (auto u : PrimeFactors[i])
-        {
-            cout << u << ' ';
-        }
-        cout << endl;
-    }
