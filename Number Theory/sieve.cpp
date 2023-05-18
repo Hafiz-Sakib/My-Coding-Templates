3a05c9f68db059e784354eb5d91547e6a69bbb14
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-18 12:47:30 +0600

    Number Theory

---
+++ Number Theory/sieve.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int mx = 1e8;
+
+bitset<mx> is_prime;
+vector<int> primes;
+
+void sieve(int n)
+{
+
+    for (int i = 3; i <= n; i += 2)
+    {
+        is_prime[i] = 1;
+    }
+
+    for (int i = 3; (i * i) <= n; i += 2)
+    {
+        if (is_prime[i])
+        {
+            for (int j = (i * i); j <= n; j += (i + i))
+            {
+                is_prime[j] = 0;
+            }
+        }
+    }
+
+    is_prime[1] = 0;
+    is_prime[2] = 1;
+    primes.push_back(2);
+
+    for (int i = 3; i <= n; i += 2)
+    {
+        if (is_prime[i])
+        {
+            primes.push_back(i);
+        }
+    }
+}
+
+int main()
+{
+    sieve(101000);
+    return 0;
+}
